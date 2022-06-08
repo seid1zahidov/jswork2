6697c3ef198770ff79c3eb3ef2c3a0d7166fafd2
Seid <101628221+seid1zahidov@users.noreply.github.com>
2022-06-09 02:27:40 +0400

    jswork2

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// function yas() {
+//     let person = prompt("Yasinizi daxil edin");
+//     if (person => 18) {
+//       document.getElementById("demo").innerHTML =
+//       "tamam" + person + "- in varsa prava ala bilersen";
+//     }
+//     else{
+//         document.getElementById("demo").innerHTML = "get boyuyende gel"
+//     }
+    
+//   }
+
+
+
+// if(confirm('bu saytda baxmaq isdiyirsen') === true){
+//     alert('xosgeldin')
+// }
+// else{
+//     alert('olmaz')
+//     document.body.style.display = 'none'
+// }
+
+function Light(){
+    var Modes = document.body
+    var test = document.body
+    if(test.style.background === 'black'){
+        test.style.background = 'white'
+        Modes.className = 'fa-solid fa-moon'
+    }
+    else{
+        test.style.background = 'black'
+        test.style.color = 'orange'
+        Modes.className = '<i class="fa-solid fa-sun"></i>'
+    }
+}
+
+
+
+
